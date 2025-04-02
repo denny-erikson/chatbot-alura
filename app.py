333868a2ca70e5d508e42578ec7dd5a96ee46ba6
Denny Erikson <denny.erikson@gmail.com>
2025-04-02 08:47:44 -0300

    implement chatbot initialization with persona and context handling

--- app.py
+++ app.py
@@ -17,6 +17,46 @@ app.secret_key = 'alura'
 
 contexto = carrega("dados/musimart.txt")
 
+def criar_chatbot():
+    personalidade = "neutro"
+
+    prompt_do_sistema = f"""
+    # PERSONA
+
+    Você é um chatbot de atendimento a clientes de um e-commerce.
+    Você não deve responder perguntas que não sejam dados do ecommerce informado!
+
+    Você deve utilizar apenas dados que estejam dentro do 'contexto'
+
+    # CONTEXTO
+    {contexto}
+
+    # PERSONALIDADE
+    {personalidade}
+
+    # Histórico
+    Acesse sempre o histórico de mensagens, e recupere informações ditas anteriormente.
+    """
+
+    configuracao_modelo = {
+        "temperature" : 0.1,
+        "max_output_tokens" : 8192
+    }
+
+    llm = genai.GenerativeModel(
+        model_name=MODELO_ESCOLHIDO,
+        system_instruction=prompt_do_sistema,
+        generation_config=configuracao_modelo
+    )
+
+    chatbot = llm.start_chat(history=[])
+
+    return chatbot
+
+
+chatbot = criar_chatbot()
+    
+
 
 def bot(prompt):
     maximo_tentativas = 1
@@ -26,33 +66,16 @@ def bot(prompt):
         try:
             personalidade = personas[selecionar_persona(prompt)]
 
-            prompt_do_sistema = f"""
-                # PERSONA
-
-                Você é um chatbot de atendimento a clientes de um e-commerce. 
-                Você não deve responder perguntas que não sejam dados do ecommerce informado!
-
-                Você deve utilizar apenas dados que estejam dentro do 'contexto'
-
-                # CONTEXTO
-                {contexto}
-
-                # PERSONALIDADE
+            mensagem_usuario = f"""
+                Considere esta personalidade para responder a mensagem:
                 {personalidade}
+                
+                Responda a seguinte mensagem, sempre lembrando do histórico:
+                {prompt}
             """
 
-            configuracao_modelo = {
-                "temperature" : 0.1,
-                "max_output_tokens" : 8192
-            }
-
-            llm = genai.GenerativeModel(
-                model_name=MODELO_ESCOLHIDO,
-                system_instruction=prompt_do_sistema,
-                generation_config=configuracao_modelo
-            )
-
-            resposta = llm.generate_content(prompt)
+            resposta = chatbot.send_message(mensagem_usuario)
+            
             return resposta.text
         except Exception as erro:
             repeticao += 1
